2ed4af0b64f75fb51b1b380097f2003eec050837
Alex Mozingo <alexmozingo@Alexs-Air.localdomain>
2021-06-07 11:38:57 -0400

    combine

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>
+            Productivity
+        </title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+
+    </head>
+    
+
+    
+
+    <body>
+        <table id="ful">
+            <tr>
+                <th>Name</th>
+                <th>Value</th>
+                <th>Quantity</th>
+            </tr>
+
+
+        </table>
+
+
+
+
+    </body>
+
+
+</html>
